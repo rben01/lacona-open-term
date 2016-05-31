6f3a230a09c988e648dd3793528e7603b587201e
Robert <rbennett@Roberts-MBP.fios-router.home>
2016-05-31 17:34:09 -0400

    First version that actually works

--- src/extensions.jsx
+++ src/extensions.jsx
@@ -1,16 +1,24 @@
 /** @jsx createElement */
 import { createElement } from 'elliptical'
 import { Command, File } from 'lacona-phrases'
-import { callSystem } from 'lacona-api'
+import { runApplescript, callSystem } from 'lacona-api'
 
 export const OpenTerminal = {
   extends: [Command],
 
   execute (result) {
-    console.log('opening term at' + result)
+		var fp = result.filepath
+		var cmd = (
+			'if [[ -d "' + fp + '" ]]; then ' +
+				'open -a Terminal "' + fp + '"; ' +
+			'elif [[ -f "' + fp + '" ]]; then ' +
+				'open -a Terminal "$(dirname "' + fp + '")"; ' + 
+			'else ' + 
+				'osascript -e \'display notification "Failed to run Lacona command"\'; ' +
+			'fi '
+		)
 
-		script = 'if [[ -d "${result}" ]]; then open -a Terminal "${result}" elif [[ -f "${result}" ]]; then open -a Terminal "$(dirname "${result}")" else osascript -e \'display notification "Could not run Lacona command"\' fi '
-    callSystem({command: script, args: []}, function() {})
+		callSystem({command: "/bin/bash", args: ['-c', cmd]}, function(){})
   },
 
   describe () {
@@ -20,9 +28,8 @@ export const OpenTerminal = {
 						<literal text='open terminal ' />
 						<literal text='terminal ' />
 					</choice>
-					<literal text='window ' />
-					<literal text='at ' optional preferred />
-					<File />
+					<literal text='window at ' />
+					<File id='filepath' />
 				</sequence>
     )
   }
